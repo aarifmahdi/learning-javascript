730c266e0a771145af33bdf61dae96ed13080818
aarifmahdi <146660683+aarifmahdi@users.noreply.github.com>
2025-08-19 07:58:41 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = "hitler"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = "hehe"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let number = 33
+
+let stringNumber = String(number)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
