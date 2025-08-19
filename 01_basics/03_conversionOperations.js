761516bdfbe566dbcb97e724a3cb11829b162e16
aarifmahdi <146660683+aarifmahdi@users.noreply.github.com>
2025-08-19 08:29:09 +0000

    conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,5 +17,48 @@ let number = 33
 
 let stringNumber = String(number)
 
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+let value = 8
+
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2%3)
+
+let str1 = "abu"
+let str2 = " huraira"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log(3 + "2")
+// console.log("3" + 2)
+
+// console.log("1" + 3 + 8)
+// console.log(1 + 3 + "8")
+
+// console.log(typeof null)  // null is an oject datatype ; just wanted to try it randomly
+
+// console.log(+true)
+// console.log(+"")
+// console.log(+false)
+// console.log(+null)
+
+let number1 = 100
+
+// number1++ OR
+++number1
+
+// console.log(number1)
+
+let a = 5;
+let b = a++;
+// console.log(`a:${a}, b:${b}`)
+
+let c = 9;
+let d = ++c;
+console.log(`c: ${c}, d: ${d}`)
